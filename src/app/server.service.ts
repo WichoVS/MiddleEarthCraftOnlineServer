8c5d027d4424fb37e2d1b3330b40de7a936eaff7
Luis Villanueva <luis.villanueva.1029@gmail.com>
2021-04-15 23:36:45 -0500

    version0.9

---
+++ src/app/server.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import{Players, Version, RootObject} from 'src/app/core/interfaces/Clases'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServerService {
+  maxP: string = '';
+  conP: string = '';
+  url: string = 'https://api.minetools.eu/ping/51.81.171.171/25582';
+  items = [];
+  obj: RootObject;
+
+  constructor(private _http: HttpClient) {
+    this._http
+      .get(this.url)
+      .toPromise()
+      .then((data) => {
+        for (let key in data)
+          if (data.hasOwnProperty(key))
+          this.items.push(data[key]);
+        console.log(this.items);
+      });
+  }
+
+  setMaxP(players: string) {
+    this.maxP = players;
+  }
+
+  setConP(players: string) {
+    this.conP = players;
+  }
+
+  getData() {
+    return this.items;
+  }
+}
